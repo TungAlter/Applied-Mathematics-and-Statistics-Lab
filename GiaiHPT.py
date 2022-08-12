0899a3229a93401a60195cec2be45b9e4a7e49e1
Lê Phan Duy Tùng <81786216+TungAlter@users.noreply.github.com>
2022-08-12 14:13:18 +0700

    Update Final

--- GiaiHPT.py
+++ GiaiHPT.py
@@ -132,11 +132,11 @@ def back_substitution(A):
 # A = [[1,-2,3],[2,2,0],[0,-3,4],[1,0,1]]
 # B = [-3,0,1,-1]
 
-A = [[1,-1,1,-3],[2,-1,4,-2]]
-B = [0,0]
-R = Gauss(A,B)
-printMatrix(R)
-back_substitution(R)
+# A = [[1,-1,1,-3],[2,-1,4,-2]]
+# B = [0,0]
+# R = Gauss(A,B)
+# printMatrix(R)
+# back_substitution(R)
 
 #print(R)
 
